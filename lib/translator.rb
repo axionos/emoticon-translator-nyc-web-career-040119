f04e9528b51c15070d8755b584d8649e959ee147
SOYEONG OH <axionoso@gmail.com>
2019-03-29 20:20:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(emoticons)
   list = YAML.load('lib/emoticons.yml')
   result = {"get_meaning" => {}, "get_emoticon" => {}}
   list.each do |meaning, array|
-
+    result["get_meaning"][array[1]] = meaning
   end
 
 
