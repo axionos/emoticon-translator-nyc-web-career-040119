188c2e46bcfcc5a0f8d74e3e9fff5fbf56709cfd
SOYEONG OH <axionoso@gmail.com>
2019-03-29 20:04:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,11 @@ emoticons = YAML.load('lib/emoticons.yml')
 def load_library(emoticons)
   emoticons
   result = {}
-  result["meaning"] = {}
+  result["mean"] = {}
   result["emoticon"] = {}
-
+  emoticons.each do ||
+    
+  end
 end
 
 def get_japanese_emoticon
