bbeee05fa5ad34d9fbfe73d3571bccc2b47c07b2
SOYEONG OH <axionoso@gmail.com>
2019-03-29 20:13:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(emoticons)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
 
   emoticons.each do |meaning, array|
-    meaning =result["get_meaning"][array[1]]
+    meaning = result["get_meaning"][array[1]]
   end
 
   result
